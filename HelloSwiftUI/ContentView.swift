8a93cf181228053580983936c18f0b25b9080e35
kaka0913 <kabuyuu0913@gmail.com>
2024-03-10 23:44:45 +0900

    Initial Commit

---
+++ HelloSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HelloSwiftUI
+//
+//  Created by 株丹優一郎 on 2024/03/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
