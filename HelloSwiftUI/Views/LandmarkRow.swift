c561fa375c08854ac0843158984ed30d0d258cd3
kaka0913 <kabuyuu0913@gmail.com>
2024-03-12 20:55:15 +0900

    HUI section1

--- HelloSwiftUI/Views/LandmarkRow.swift
+++ HelloSwiftUI/Views/LandmarkRow.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct LandmarkRow: View {
+    
     var landmark: Landmark
     
     var body: some View {
@@ -17,6 +18,10 @@ struct LandmarkRow: View {
                 .frame(width: 50, height: 50)
             Text(landmark.name)
             Spacer()
+            if landmark.isFavorite{
+                Image(systemName: "star.fill")
+                    .foregroundStyle(.yellow)
+            }
         }
     }
 }
