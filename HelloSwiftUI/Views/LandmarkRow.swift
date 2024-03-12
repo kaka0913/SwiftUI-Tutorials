f999b0117bc28257af2c6652ded42787f25609f9
kaka0913 <kabuyuu0913@gmail.com>
2024-03-12 14:50:46 +0900

    リスト第１節

---
+++ HelloSwiftUI/Views/LandmarkRow.swift
@@ -0,0 +1,18 @@
+//
+//  LandmarkRow.swift
+//  HelloSwiftUI
+//
+//  Created by 株丹優一郎 on 2024/03/12.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LandmarkRow()
+}
--- ModelData.swift
+++
@@ -1,8 +0,0 @@
-//
-//  ModelData.swift
-//  HelloSwiftUI
-//
-//  Created by 株丹優一郎 on 2024/03/12.
-//
-
-import Foundation
