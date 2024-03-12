e96c1c971dcfa5c465b279bcb27f84c0f2e67d37
kaka0913 <kabuyuu0913@gmail.com>
2024-03-12 19:43:09 +0900

    セクション８

--- HelloSwiftUI/Views/CircleImage.swift
+++ HelloSwiftUI/Views/CircleImage.swift
@@ -8,8 +8,9 @@
 import SwiftUI
 
 struct CircleImage: View {
+    var image: Image
     var body: some View {
-        Image("呪術")
+        image
             .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
             .overlay{
                 Circle().stroke(.white, lineWidth: 4)
@@ -20,5 +21,5 @@ struct CircleImage: View {
     
 
 #Preview {
-    CircleImage()
+    CircleImage(image: Image("turtlerock"))
 }
--- HelloSwiftUI/Views/LandmarkDetail.swift
+++ HelloSwiftUI/Views/LandmarkDetail.swift
@@ -8,46 +8,51 @@
 import SwiftUI
 
 struct LandmarkDetail: View {
+    var landmark: Landmark
+    
     var body: some View {
-        VStack {
-            MapView()
-                .frame(height: 300)
+        ScrollView {
+                MapView(coordinate: landmark.locationCoordinate)
+                    .frame(height: 300)
 
 
-            CircleImage()
-                .offset(y: -130)
-                .padding(.bottom, -130)
+                CircleImage(image: landmark.image)
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
 
 
-            VStack(alignment: .leading) {
-                Text("Turtle Rock")
-                    .font(.title)
+                VStack(alignment: .leading) {
+                    Text("Turtle Rock")
+                        .font(.title)
 
 
-                HStack {
-                    Text("Joshua Tree National Park")
-                    Spacer()
-                    Text("California")
+                    HStack {
+                        Text(landmark.name)
+                        Spacer()
+                        Text("California")
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.secondary)
+
+
+                    Divider()
+
+
+                    Text("About \(landmark.name)")
+                        .font(.title2)
+                    Text(landmark.description)
                 }
-                .font(.subheadline)
-                .foregroundStyle(.secondary)
+                .padding()
 
 
-                Divider()
-
-
-                Text("About Turtle Rock")
-                    .font(.title2)
-                Text("Descriptive text goes here.")
-            }
-            .padding()
-
-
-            Spacer()
+                Spacer()
+            
         }
+        .navigationTitle(landmark.name)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 #Preview {
-    LandmarkDetail()
+    LandmarkDetail(landmark: landmarks[0])
 }
