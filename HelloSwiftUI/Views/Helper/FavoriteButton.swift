b0d8335ee84707041dc205dfd466cf7992383bad
kaka0913 <kabuyuu0913@gmail.com>
2024-03-15 01:00:26 +0900

    ディレクトリ階層変更

---
+++ HelloSwiftUI/Views/Helper/FavoriteButton.swift
@@ -0,0 +1,28 @@
+//
+//  FavoriteButton.swift
+//  HelloSwiftUI
+//
+//  Created by 株丹優一郎 on 2024/03/15.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+
+
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
