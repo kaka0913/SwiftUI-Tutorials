178ea2817ac07fb9c6ed806a99f71450f661757b
kaka0913 <kabuyuu0913@gmail.com>
2024-03-18 13:28:39 +0900

    特徴カード

---
+++ HelloSwiftUI/Views/PageView/PageView.swift
@@ -0,0 +1,18 @@
+//
+//  PageView.swift
+//  HelloSwiftUI
+//
+//  Created by 株丹優一郎 on 2024/03/18.
+//
+
+import SwiftUI
+
+struct PageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageView()
+}
